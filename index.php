999698e93a9884db55928b0293b71de077f22774
AndreVictor60 <andre.victor60@free.fr>
2017-10-02 10:40:18 +0200

    Modification de la fonction

--- index.php
+++ index.php
@@ -2,21 +2,17 @@
 // fonction qui prend en paramètre un tableau et qui retourne une chaîne avec la fusion de toutes les lignes du tableau.
 function fusion($Tabl){
   //Rassemble les éléments d'un tableau en une chaîne
-    echo implode($Tabl);
+    return implode($Tabl);
 }
 //fonction qui prend un tableau en paramètre et qui retourne un tableau trié par ordre alphabétique.
 function trieAlpha($Tabl){
   // Trie un tableau
   sort($Tabl);
-  // foreach = parcouris un tableau
-  // [0]=> HTML, [1]=> CSS, [2]=> Javascript, [3]=>PHP
-  foreach ($Tabl as $key => $value) {
-    echo "$value  \n";
-  }
+  return $Tabl;
 }
 // init du tableau langage
 $langage = array("HTML","CSS","Javascript","PHP");
 // Appels les fonctions
-trieAlpha($langage);
-fusion($langage);
+print_r(trieAlpha($langage));
+echo fusion($langage);
 ?>
